21d8a9abd9025bd0bd5eced3699403b3f8d71028
SirWadefield <sirwadefield@gmail.com>
2018-09-01 21:16:39 -0500

    created random number generator and interactivity with such.

--- classwork1/Program.cs
+++ classwork1/Program.cs
@@ -12,6 +12,39 @@ namespace classwork1
         {
             Console.WriteLine("hello world!");
             Console.ReadLine();
+            Random rand = new Random();
+            int r = rand.Next(1, 100);
+            //Console.WriteLine(r);
+            Console.WriteLine();
+            int guess = 0;
+            int guess_amt = 0;
+            while (guess != r)
+            {
+                Console.WriteLine("What's my number?(between 1 and 100)");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == r)
+                {
+                    Console.WriteLine(guess + " is correct!");
+                    guess_amt++;
+                    Console.WriteLine("it took you " + guess_amt + " tries.");
+                    Console.WriteLine("press enter to exit");
+                    Console.ReadLine();
+                }
+
+                else if (guess < r)
+                {
+                    Console.WriteLine(guess + " is too low!");
+                    guess_amt++;
+                }
+
+                else if (guess > r)
+                {
+                    Console.WriteLine(guess + " is too high!");
+                    guess_amt++;
+                }
+            }
+            
         }
     }
 }
